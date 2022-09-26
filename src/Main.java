cebdd9d5a17f3ee5c9ba7681bf46daf915c1d43d
zemscky <aleksei.zemscky@yandex.ru>
2022-09-26 15:45:31 +0300

    Задание 1,2 Исправления

--- src/Main.java
+++ src/Main.java
@@ -2,23 +2,31 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        task_1();
-        task_2();
+        task();
     }
 
-    public static void task_1() {
+    public static void task() {
         System.out.println("Задание 1");
-        double[] weight = new double[]{1.57, 7.654, 9.986};
-        for (int i = 0; i < weight.length; i++) {
-            System.out.println(weight[i]);
+        // 1
+        System.out.println("Массив 1");
+        int[] number = new int[3];
+        number[0] = 1;
+        number[1] = 2;
+        number[2] = 3;
+        for (int i = 0; i < 3; i++) {
+            System.out.println(number[i]);
         }
-    }
+        // 2
+        System.out.println("Массив 2");
+        double[] weights = {1.57, 7.654, 9.986};
+        for (int i = 0; i < weights.length; i++) {
+            System.out.println(weights[i]);
+        }
+        // 3
+        System.out.println("Массив 3");
+        int[] yearOfBirth = {1990, 1995, 2000, 2005, 2010};
+        System.out.println(yearOfBirth[0]);
 
-    public static void task_2() {
-        System.out.println("Задание 2");
-        double[] weight = new double[]{1.57, 7.654, 9.986};
-         {
-            System.out.print(weight[0] + ", " + weight[1] +", "+ weight[2]);
-        }
+
     }
 }
