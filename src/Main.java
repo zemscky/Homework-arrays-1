f7d270df5e67d7817244d49e85e08cb6b3d6cd69
zemscky <aleksei.zemscky@yandex.ru>
2022-09-26 14:17:54 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-
+        task_1();
+    }
+    public static void task_1() {
+        System.out.println("Задание 1");
+        double[] weight = new double[]{1.57, 7.654, 9.986};
+        for (int i = 0; i < weight.length; i++) {
+            System.out.println(weight[i]);
+        }
     }
 }
