c67e1b9f405aefd306518893b5e6975cfdf96bf2
zemscky <aleksei.zemscky@yandex.ru>
2022-09-27 12:14:37 +0300

    Исправления

--- src/Main.java
+++ src/Main.java
@@ -6,65 +6,86 @@ public class Main {
     public static void main(String[] args) {
         task_1();
         task_2();
+        task_3();
+        task_4();
+    }
+    public static void task_1() {
+        System.out.println("Задание 1");
+        int[] number = new int[3]; // 1 массив
+        number[0] = 1;
+        number[1] = 2;
+        number[2] = 3;
+
+        double[] weights = {1.57, 7.654, 9.986}; // 2 массив
+
+        int[] year = {1990, 1995, 2000, 2005, 2010}; // 3 массив
     }
 
-    public static void task_1 () {
-        System.out.println("Задание 1");
-        System.out.println("Массив 1");
+    public static void task_2() {
+        System.out.println("Задание 2");
         int[] number = new int[3];
         number[0] = 1;
         number[1] = 2;
         number[2] = 3;
-        System.out.println("Массив 2");
-        double[] weights = {1.57, 7.654, 9.986}; // 2 массив
-        System.out.println("Массив 3");
-        int[] year = {1990, 1995, 2000, 2005, 2010}; // 3 массив
-
-        System.out.println("Задание 2");
         for (int i = 0; i < number.length; i++) {
-            System.out.print(number [i]);
-            if (i != number.length -1){
+            System.out.print(number[i]);
+            if (i != number.length - 1) {
                 System.out.print(", ");
             }
         }
         System.out.println();
+
+        double[] weights = {1.57, 7.654, 9.986};
         for (int i = 0; i < weights.length; i++) {
-            System.out.print(weights [i]);
-            if (i != weights.length -1){
+            System.out.print(weights[i]);
+            if (i != weights.length - 1) {
                 System.out.print(", ");
             }
         }
         System.out.println();
+
+        int[] year = {1990, 1995, 2000, 2005, 2010};
         for (int i = 0; i < year.length; i++) {
-            System.out.print(year [i]);
-            if (i != year.length -1){
+            System.out.print(year[i]);
+            if (i != year.length - 1) {
                 System.out.print(", ");
             }
         }
         System.out.println();
-
+    }
+    public static void task_3() {
         System.out.println("Задание 3");
-
+        int[] number = new int[3];
+        number[0] = 1;
+        number[1] = 2;
+        number[2] = 3;
         for (int i = number.length - 1; i >= 0; i--) {
             System.out.print(number [i]);
             if (i != 0) {
                 System.out.print(", ");
             }
-        }System.out.println();
+        }
+        System.out.println();
+
+        double[] weights = {1.57, 7.654, 9.986};
         for (int i = weights.length - 1; i >= 0; i--) {
             System.out.print(weights [i]);
             if (i != 0) {
                 System.out.print(", ");
             }
-        }System.out.println();
+        }
+        System.out.println();
+
+        int[] year = {1990, 1995, 2000, 2005, 2010};
         for (int i = year.length - 1; i >= 0; i--) {
             System.out.print(year [i]);
             if (i != 0) {
                 System.out.print(", ");
             }
-        }System.out.println();
+        }
+        System.out.println();
     }
-    public static void task_2() {
+    public static void task_4() {
         System.out.println("Задание 4");
         int[] number = new int[3];
         number[0] = 1;
@@ -74,7 +95,6 @@ public class Main {
             if (number[i] % 2 != 0) {
                 number[i] += 1;
             }
-
         }
         System.out.println(Arrays.toString(number));
     }
